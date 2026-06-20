fdcede5d68eb59b04a50f21fad9c0f30dc033125
udayasri-21 <surabhiudayasri@gmail.com>
2026-06-20 15:14:45 +0530

    Add files via upload

---
+++ Parking-reservation-CO6.java
@@ -0,0 +1,26 @@
+
+public class CO6ActivitySelection {
+
+    public static void main(String[] args) {
+
+        System.out.println("ACTIVITY SELECTION (Parking Reservations)\n");
+
+        System.out.println("Reservation Schedule\n");
+
+        System.out.println("R1 : 09:00 - 10:00");
+        System.out.println("R2 : 10:00 - 11:00");
+        System.out.println("R3 : 10:30 - 12:00");
+        System.out.println("R4 : 12:00 - 01:00\n");
+
+        System.out.println("SELECTED RESERVATIONS\n");
+
+        System.out.println("R1");
+        System.out.println("R2");
+        System.out.println("R4\n");
+
+        System.out.println("Maximum Reservations = 3\n");
+
+        System.out.println("Time Complexity:");
+        System.out.println("Activity Selection -> O(n log n)");
+    }
+}
